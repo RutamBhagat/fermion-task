5614347404afcf38ad2557c5d7df878cdf98cbe8
Rutam Bhagat <rutambhagat@gmail.com>
2025-08-02 01:59:30 +0530

    refactor: clean up ecosystem.config.cjs by removing comments and ensuring consistent formatting

--- apps/server/ecosystem.config.cjs
+++ apps/server/ecosystem.config.cjs
@@ -1,9 +1,8 @@
-// apps/server/ecosystem.config.js
 module.exports = {
   apps: [
     {
-      name: 'hono-backend',      // The name of your application
-      script: 'dist/index.js',  // The entry point of your built app
+      name: "hono-backend",
+      script: "dist/index.js",
       instances: 1,
       autorestart: true,
       watch: false,
