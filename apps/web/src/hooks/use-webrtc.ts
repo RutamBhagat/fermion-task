aa07d1bdeb8ffa4e4943ac02c10ede92ed4a38b0
Rutam Bhagat <rutambhagat@gmail.com>
2025-08-04 20:10:21 +0530

    feat: update useWebRTC hook with consumer transport creation

--- apps/web/src/hooks/use-webrtc.ts
+++ apps/web/src/hooks/use-webrtc.ts
@@ -1,14 +1,30 @@
 "use client";
 
 import { Device } from "mediasoup-client";
-import type { RtpCapabilities } from "mediasoup-client/types";
+import type {
+  RtpCapabilities,
+  Transport,
+  DtlsParameters,
+  IceCandidate,
+  IceParameters,
+} from "mediasoup-client/types";
 import { useCallback, useRef, useState } from "react";
 import type { Socket } from "socket.io-client";
+
+interface TransportParams {
+  params: {
+    id: string;
+    iceParameters: IceParameters;
+    iceCandidates: IceCandidate[];
+    dtlsParameters: DtlsParameters;
+  };
+}
 
 export function useWebRTC() {
   const [isProducing, setIsProducing] = useState(false);
 
   const deviceRef = useRef<Device | null>(null);
+  const consumerTransportRef = useRef<Transport | null>(null);
 
   const initializeDevice = useCallback(async (socket: Socket) => {
     const { rtpCapabilities } = await new Promise<{
@@ -27,9 +43,33 @@ export function useWebRTC() {
     return device;
   }, []);
 
+  const createConsumerTransport = useCallback(async (socket: Socket) => {
+    if (!deviceRef.current) return;
+
+    const { params } = await new Promise<TransportParams>((resolve) => {
+      socket.emit("createWebRtcTransport", { type: "consumer" }, resolve);
+    });
+
+    const consumerTransport = deviceRef.current.createRecvTransport(params);
+    consumerTransportRef.current = consumerTransport;
+
+    consumerTransport.on("connect", async ({ dtlsParameters }, callback) => {
+      socket.emit(
+        "connectTransport",
+        {
+          transportId: consumerTransport.id,
+          dtlsParameters,
+        },
+        callback
+      );
+    });
+
+    return consumerTransport;
+  }, []);
 
   return {
     isProducing,
     initializeDevice,
+    createConsumerTransport,
   };
 }
