f702766e7ac70f48c414fad054fb19ab64a8111e
Rutam Bhagat <rutambhagat@gmail.com>
2025-08-05 19:17:37 +0530

    feat: add RoomState interface

--- apps/server/src/types/index.ts
+++ apps/server/src/types/index.ts
@@ -1,6 +1,19 @@
-import type { WebRtcTransport } from "mediasoup/types";
+import type {
+  Consumer,
+  Producer,
+  Router,
+  WebRtcTransport,
+} from "mediasoup/types";
 
 export interface SocketTransports {
   producer?: WebRtcTransport;
   consumer?: WebRtcTransport;
+}
+
+export interface RoomState {
+  router: Router;
+  participants: Set<string>;
+  transports: Map<string, SocketTransports>;
+  producers: Map<string, Producer[]>;
+  consumers: Map<string, Consumer>;
 }
