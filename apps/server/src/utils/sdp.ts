84fc68e1e69656e2591858b0e615d6d11b7c9a9e
Rutam Bhagat <rutambhagat@gmail.com>
2025-08-07 04:09:30 +0530

    refactor: optimize generateCompositeSDP function by using array for SDP parts

--- apps/server/src/utils/sdp.ts
+++ apps/server/src/utils/sdp.ts
@@ -5,33 +5,39 @@ export function generateCompositeSDP(
   videoConsumers: Consumer[],
   basePort: number
 ): string {
-  let sdp = "v=0\r\n";
-  sdp += "o=- 0 0 IN IP4 127.0.0.1\r\n";
-  sdp += "s=FFmpeg\r\n";
-  sdp += "c=IN IP4 127.0.0.1\r\n";
-  sdp += "t=0 0\r\n";
+  const sdpParts = [
+    "v=0",
+    "o=- 0 0 IN IP4 127.0.0.1",
+    "s=FFmpeg",
+    "c=IN IP4 127.0.0.1",
+    "t=0 0",
+  ];
 
   let currentPort = basePort;
 
   audioConsumers.forEach((consumer) => {
     const codec = consumer.rtpParameters.codecs[0];
-    sdp += `m=audio ${currentPort} RTP/AVP ${codec.payloadType}\r\n`;
-    sdp += `a=rtpmap:${codec.payloadType} ${codec.mimeType.split("/")[1]}/${
-      codec.clockRate
-    }/${codec.channels}\r\n`;
-    sdp += "a=sendonly\r\n";
+    sdpParts.push(
+      `m=audio ${currentPort} RTP/AVP ${codec.payloadType}`,
+      `a=rtpmap:${codec.payloadType} ${codec.mimeType.split("/")[1]}/${
+        codec.clockRate
+      }/${codec.channels}`,
+      "a=sendonly"
+    );
     currentPort += 2;
   });
 
   videoConsumers.forEach((consumer) => {
     const codec = consumer.rtpParameters.codecs[0];
-    sdp += `m=video ${currentPort} RTP/AVP ${codec.payloadType}\r\n`;
-    sdp += `a=rtpmap:${codec.payloadType} ${codec.mimeType.split("/")[1]}/${
-      codec.clockRate
-    }\r\n`;
-    sdp += "a=sendonly\r\n";
+    sdpParts.push(
+      `m=video ${currentPort} RTP/AVP ${codec.payloadType}`,
+      `a=rtpmap:${codec.payloadType} ${codec.mimeType.split("/")[1]}/${
+        codec.clockRate
+      }`,
+      "a=sendonly"
+    );
     currentPort += 2;
   });
 
-  return sdp;
+  return sdpParts.join("\r\n") + "\r\n";
 }
