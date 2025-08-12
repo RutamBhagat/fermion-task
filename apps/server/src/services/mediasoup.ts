c4b73a24686753f6c131bbc91cbc84c6680f24c3
Rutam Bhagat <rutambhagat@gmail.com>
2025-08-12 11:44:44 +0530

    fix: reorder imports and update log message in mediasoup initialization

--- apps/server/src/services/mediasoup.ts
+++ apps/server/src/services/mediasoup.ts
@@ -1,12 +1,12 @@
 import type { Worker } from "mediasoup/types";
-import { workerSettings } from "@/config/mediasoup";
 import { createWorker } from "mediasoup";
+import { workerSettings } from "@/config/mediasoup";
 
 let worker: Worker;
 
 export async function initMediasoup() {
   worker = await createWorker(workerSettings);
-  console.log("Mediasoup worker and legacy router initialized");
+  console.log("Mediasoup worker initialized");
 }
 
 export function getWorker(): Worker {
